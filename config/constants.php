966676c3e5c80f55916d54e7661d0be426506c82
harshan dilantha <harshan.dilantha@gmail.com>
2019-08-02 23:58:45 +0530

    constant of item type

--- config/constants.php
+++ config/constants.php
@@ -4,5 +4,11 @@ return [
     'status' => [
         'Active' => '0',
         'Inactive' => '1',
+    ],
+    'unit_put' => [
+        '1' => 'm',
+        '2' => 'PC',
+        '3' => 'kg',
     ]
+
 ];
