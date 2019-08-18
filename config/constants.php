be4510b87df6b31aab9599dfc4d9153958dd527d
harshan dilantha <harshan.dilantha@gmail.com>
2019-08-18 23:37:35 +0530

    constant add for po status

--- config/constants.php
+++ config/constants.php
@@ -9,6 +9,16 @@ return [
         '1' => 'm',
         '2' => 'PC',
         '3' => 'kg',
+    ], 'po_status' => [
+        'Received' => '1',
+        'Ordered' => '2',
+        'Pending' => '3',
+        'Canceled' => '4',
+    ], 'po_status_to_name' => [
+        '1' => 'Received',
+        '2' => 'Ordered',
+        '3' => 'Pending',
+        '4' => 'Canceled',
     ]
 
 ];
