8622c3bae1df018b10612f8c81f3cdc44f0876c4
harshan dilantha <harshan.dilantha@gmail.com>
2019-08-22 23:16:02 +0530

    stock table fillable colums declare and 2 queries added fro count stock location wisely

--- app/Stock.php
+++ app/Stock.php
@@ -8,17 +8,28 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Stock extends Model
 {
     protected $table = 'stock';
+//    protected $primaryKey = 'receive_code'; // or null
     use SoftDeletes;
+    protected $fillable = ['receive_code', 'receive_date', 'remarks','location'];
 
     function stockItems()
     {
         return $this->hasMany(StockItems::class, 'stock_id', 'id');
     }
 
-    function qtySum()
+    function qtyAddedSum()
     {
         return $this->hasMany(StockItems::class, 'stock_id', 'id')
             ->selectRaw('stock_items.*, sum(qty) as sum')
+            ->where('stock_items.method', '=', 'A')
+            ->havingRaw('sum > ?', [0])->groupBy('stock_items.item_id');
+    }
+
+    function qtySubsSum()
+    {
+        return $this->hasMany(StockItems::class, 'stock_id', 'id')
+            ->selectRaw('stock_items.*, sum(qty) as sum')
+            ->where('stock_items.method', '=', 'S')
             ->havingRaw('sum > ?', [0])->groupBy('stock_items.item_id');
     }
 }
