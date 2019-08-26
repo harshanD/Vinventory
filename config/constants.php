a7aa7eb637d7b0aadefe6f9554afcc496abd8f03
harshan dilantha <harshan.dilantha@gmail.com>
2019-08-26 23:33:22 +0530

    constants add for i_sale_status ,i_sale_status_value

--- config/constants.php
+++ config/constants.php
@@ -9,19 +9,36 @@ return [
         '1' => 'm',
         '2' => 'PC',
         '3' => 'kg',
-    ], 'po_status' => [
+    ],
+    'po_status' => [
         'Received' => '1',
         'Ordered' => '2',
         'Pending' => '3',
         'Canceled' => '4',
-    ], 'po_status_to_name' => [
+    ],
+    'po_status_to_name' => [
         '1' => 'Received',
         '2' => 'Ordered',
         '3' => 'Pending',
         '4' => 'Canceled',
-    ], 'taxActive' => [
+    ],
+    'taxActive' => [
         'Active' => '2',
         'Inactive' => '1',
+    ],
+    'i_sale_status' => [
+        'Pending' => '1',
+        'Completed' => '2',
+    ],
+    'i_sale_status_value' => [
+        '1' => 'Pending',
+        '2' => 'Completed',
+    ],
+    'i_payment_status_value' => [
+        '1' => 'Pending',
+        '2' => 'Duo',
+        '3' => 'Partial',
+        '4' => 'Paid',
     ]
 
 ];
