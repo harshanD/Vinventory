a44f195d28de6bff8f717d66cef88a252eb5719b
harshan dilantha <harshan.dilantha@gmail.com>
2019-08-31 16:16:14 +0530

    shortcut names correction

--- resources/views/vendor/adminlte/returns/edit.blade.php
+++ resources/views/vendor/adminlte/returns/edit.blade.php
@@ -42,7 +42,7 @@
         <!-- Default box -->
         <div class="box">
             <div class="box-header with-border">
-                <h3 class="box-title">Edit Transfer</h3>
+                <h3 class="box-title">Edit Returns</h3>
 
                 <div class="box-tools pull-right">
                     <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
