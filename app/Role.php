e509a0bf45fc96bf41ee0efa8401b70e84d3ea03
harshan dilantha <harshan.dilantha@gmail.com>
2019-08-31 10:11:06 +0530

    roles model class

--- app/Role.php
+++ app/Role.php
@@ -4,13 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Wildside\Userstamps\Userstamps;
 
-class Role extends Model {
+class Role extends Model
+{
 
     protected $fillable = ['name'];
     use SoftDeletes;
+    use Userstamps;
 
-    public function users() {
+    public function users()
+    {
         return $this->belongsToMany(User::class);
     }
 
